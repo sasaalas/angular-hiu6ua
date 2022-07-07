625d1b96524e7bc510913a122e4a2ef081316733
unknown <sami.saalasti@gmail.com>
2022-07-07 11:27:03 +0300

    testi commit

--- src/test.ts
+++ src/test.ts
@@ -24,10 +24,3 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().forEach(context);
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
